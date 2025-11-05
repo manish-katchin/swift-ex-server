d3876732fa233ae7f097f085d4f998f40c38383b
hunny <hunny@katchintech.com>
2025-11-05 11:41:59 +0530

    Removed trust limit and update dto.

--- src/api/v1/auth/dto/reset-password.dto.ts
+++ src/api/v1/auth/dto/reset-password.dto.ts
@@ -1,13 +1,18 @@
-import { IsEmail, IsNotEmpty } from 'class-validator';
+import { IsEmail, IsNotEmpty, Matches, MaxLength, MinLength } from 'class-validator';
 
 export class ResetPasswordDto {
   @IsEmail()
   @IsNotEmpty()
+  @MaxLength(100, { message: 'Email must not exceed 100 characters' })
   email: string;
 
   @IsNotEmpty()
   otp: number;
 
   @IsNotEmpty()
+  @MinLength(5, { message: 'Password too short' })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])^[A-Za-z\d@$!%*?&]+$/, {
+    message: 'Use format like: Ab1@00'
+  })
   password: string;
 }
